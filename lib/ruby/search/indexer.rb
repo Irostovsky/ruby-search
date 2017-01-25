4b4b36753bc88d3163e88d9d6a0c4cb52b14000d
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-01-25 17:33:04 +0200

    update index.

--- lib/ruby/search/indexer.rb
+++ lib/ruby/search/indexer.rb
@@ -1,7 +1,14 @@
 require 'yaml'
+require 'active_support/core_ext/hash'
 
 class Ruby::Search::Indexer
   attr_accessor :text
+  INDEX_STORE = 'index.yml'
+
+  def initialize
+    @current_index = File.exist?(INDEX_STORE) ? YAML.load(IO.read(INDEX_STORE)) : {}
+  end
+
   def echo
     puts 'Test'
   end
@@ -17,12 +24,13 @@ class Ruby::Search::Indexer
       return p "File #{file_name} does not exists"
     end
 
-    p @text = IO.read(file_name)
-    arr = @text.split(/\W+/)
-    p tokens = arr.length.times.map{|i| arr.length.times.map{|j| arr[i..j].join(' ') unless i > j}}.flatten.compact
-    p hash = tokens.inject({}){|h, s| h[s] ||= {file_name => 0}; h[s][file_name] += 1; h }
-    File.open("index.yml","w") do |file|
-       file.write hash.to_yaml
+    text = IO.read(file_name)
+    tokens = text.split(/\W+/)
+    # tokens = arr.length.times.map{|i| arr.length.times.map{|j| arr[i..j].join(' ') unless i > j}}.flatten.compact
+    new_index = tokens.inject({}){|h, s| h[s] ||= {file_name => 0}; h[s][file_name] += 1; h }
+    res = @current_index.deep_merge(new_index){ |key, this_val, other_val| other_val }
+    File.open(INDEX_STORE,"w") do |file|
+       file.write res.to_yaml
     end
   end
 
