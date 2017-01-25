bbd86091418aa380aa635a27307d8408411ba5af
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-01-25 13:04:09 +0200

    Simple gem solution.

--- lib/ruby/search.rb
+++ lib/ruby/search.rb
@@ -2,6 +2,10 @@ require "ruby/search/version"
 
 module Ruby
   module Search
-    # Your code goes here...
-  end
+    class Chatter
+        def say_hello
+          puts 'This is zerp. Coming in loud and clear. Over.'
+        end
+      end
+    end
 end
