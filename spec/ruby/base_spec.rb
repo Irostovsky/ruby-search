bd1c08e0dcda90ed91e1b6f2a276ad3e0b4bb6f9
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-01-27 10:43:59 +0200

    Rspec for the search logic.

--- spec/ruby/base_spec.rb
+++ spec/ruby/base_spec.rb
@@ -49,4 +49,144 @@ RSpec.describe Ruby::Search::Base do
       expect(described_class.new.send(:keywords, ['less', 'AND'])).to eq [['less']]
     end
   end
+
+  describe '#do_search' do
+
+    it 'several keywords' do
+      index = {
+        'ruby' => {
+          'file2' => 2,
+        },
+        'rails' => {
+          'file1' => 2
+        }
+      }
+      expect_search index, 'ruby', 'on', 'rails' do
+        [
+          ['ruby', ['file2', 2]],
+          ['on'],
+          ['rails', ['file1', 2]]
+        ]
+      end
+    end
+
+    it 'ordered files by count' do
+      index = {
+        'ruby' => {
+          'file2' => 2,
+          'file3' => 3,
+        }
+      }
+      expect_search index, 'ruby' do
+        [
+          ['ruby', ['file3', 3], ['file2', 2]]
+        ]
+      end
+    end
+
+    it 'search sentence, with order' do
+      index = {
+        'ruby on rails' => {
+          'file2' => 2,
+          'file3' => 3,
+        },
+        'less' => {
+          'file2' => 1
+        }
+      }
+      expect_search index, "less", "ruby on rails" do
+        [
+          ['less', ['file2', 1]],
+          ['ruby on rails', ['file3', 3], ['file2', 2]]
+        ]
+      end
+    end
+
+    context 'AND query' do
+      let(:index) do
+        {
+          'ruby' => {
+            'file2' => 2,
+            'file3' => 3,
+          },
+          'rails' => {
+            'file2' => 3,
+            'file4' => 4,
+          },
+          'less' => {
+            'file2' => 1
+          },
+          'less sass' => {
+            'file2' => 1
+          }
+        }
+      end
+      it '2 items' do
+        expect_search index, 'ruby', 'AND', 'rails' do
+          [
+            ['ruby AND rails', ['file2', 3]]
+          ]
+        end
+      end
+
+      it 'several items' do
+        expect_search index, 'ruby', 'AND', 'rails', 'AND', 'less' do
+          [
+            ['ruby AND rails AND less', ['file2', 3]]
+          ]
+        end
+      end
+
+      it 'several groups' do
+        expect_search index, 'ruby', 'AND', 'rails', 'less', 'AND', 'rails' do
+          [
+            ['ruby AND rails', ['file2', 3]],
+            ['less AND rails', ['file2', 3]]
+          ]
+        end
+      end
+
+      it 'with aditional word' do
+        expect_search index, 'ruby', 'AND', 'rails', 'less' do
+          [
+            ['ruby AND rails', ['file2', 3]],
+            ['less', ['file2', 1]]
+          ]
+        end
+      end
+
+      it 'with sentence' do
+        expect_search index, 'ruby', 'AND', 'less sass' do
+          [
+            ['ruby AND less sass', ['file2', 2]]
+          ]
+        end
+      end
+
+      it 'ignore start AND' do
+        expect_search index, 'AND', 'rails', 'less' do
+          [
+            ['rails', ['file4', 4], ['file2', 3]],
+            ['less', ['file2', 1]]
+          ]
+        end
+      end
+
+      it 'ignore end AND' do
+        expect_search index, 'rails', 'less', 'AND' do
+          [
+            ['rails', ['file4', 4], ['file2', 3]],
+            ['less', ['file2', 1]]
+          ]
+        end
+      end
+
+    end
+  end
+
+  def expect_search index, *search_args, &block
+    search = described_class.new
+    search.instance_variable_set :@current_index, index
+    expect(search.send(:do_search, search_args)).to eq(block.call)
+  end
 end
