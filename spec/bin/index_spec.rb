c7991b83d4cc65479497485997e1fc3ddd3dcbf1
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-01-27 13:18:30 +0200

    Test the cli index.

--- spec/bin/index_spec.rb
+++ spec/bin/index_spec.rb
@@ -1,11 +1,23 @@
 require "spec_helper"
 
 RSpec.describe Ruby::Search do
-  it 'check index without args' do
-    setup_aruba
-    run "bin/index"
+  describe '#index' do
 
-    stop_all_commands
-    expect(last_command_started.output).to eq("No file path passed\n")
+    it 'check index without args' do
+      setup_aruba
+      run "bin/index"
+
+      stop_all_commands
+      expect(last_command_started.output).to eq("No file path passed\n")
+    end
+
+    it 'check index text file' do
+      setup_aruba
+      run "bin/index ../../spec/tmp/file1.txt"
+
+      stop_all_commands
+      expect(last_command_started.output).to eq("Updated index: index.yml\n")
+    end
+
   end
 end
